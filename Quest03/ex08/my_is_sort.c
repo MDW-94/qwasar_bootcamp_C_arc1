0fe50da6f147faf8d362e08f31c6039fcfe29b98
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-02-20 18:17:02 +0000

    assignments finished for this qwasar quest

--- Quest03/ex08/my_is_sort.c
+++ Quest03/ex08/my_is_sort.c
@@ -23,40 +23,70 @@ bool my_is_sort(integer_array* param_1){
 
     // if(all array[i]s are > eachother by 1 || < eachother by 1) return 1 else return 0
     for(int i = 1; i < param_1->size - 1;i++){
-        if(!(param_1->array[i] > param_1->array[i - 1] && param_1->array[i] < param_1->array[i + 1] 
+        if(!(param_1->array[i] >= param_1->array[i - 1] && param_1->array[i] <= param_1->array[i + 1] 
         || 
-        param_1->array[i] < param_1->array[i - 1] && param_1->array[i] > param_1->array[i + 1])){
+        param_1->array[i] <= param_1->array[i - 1] && param_1->array[i] >= param_1->array[i + 1])){
             return false;
         }
     }
     return true;
 }
 
+// bool my_is_sort(integer_array* param_1) { // Longwinded way
+//     if (param_1->size < 2) {
+//         // An array with 0 or 1 element is always considered sorted
+//         return true;
+//     }
+
+//     // Check for ascending order
+//     if (param_1->array[0] <= param_1->array[1]) {
+//         for (int i = 1; i < param_1->size - 1; i++) {
+//             if (param_1->array[i] > param_1->array[i + 1]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     // Check for descending order
+//     else if (param_1->array[0] >= param_1->array[1]) {
+//         for (int i = 1; i < param_1->size - 1; i++) {
+//             if (param_1->array[i] < param_1->array[i + 1]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     // Array is not in sequential order
+//     else {
+//         return false;
+//     }
+
+//     return true;
+// }
+
 int test(){
 
-    int array_1[] = {1, 3, 4, 5, 6};
-    int array_2[] = {60, 50, 40, 30, 20};
-    int array_3[] = {1, 5, 3, 9, 2, 1, 4, 8};
-    int array_4[] = {2, 1, -1};
+    int array_1[] = {1, 1, 2};
+    int array_2[] = {2, 1, -1};
+    int array_3[] = {4, 7, 0, 3};
+    int array_4[] = {};
 
     integer_array test_1; // true
     integer_array* ptr_test_1 = &test_1;
-    ptr_test_1->size = 5;
+    ptr_test_1->size = 3;
     ptr_test_1->array = array_1;
 
     integer_array test_2; // true
     integer_array* ptr_test_2 = &test_2;
-    ptr_test_2->size = 5;
+    ptr_test_2->size = 3;
     ptr_test_2->array = array_2;
 
     integer_array test_3; // false
     integer_array* ptr_test_3 = &test_3;
-    ptr_test_3->size = 8;
+    ptr_test_3->size = 4;
     ptr_test_3->array = array_3;
 
     integer_array test_4; // true
     integer_array* ptr_test_4 = &test_4;
-    ptr_test_4->size = 8;
+    ptr_test_4->size = 0;
     ptr_test_4->array = array_4;
 
     if(my_is_sort(ptr_test_1)){
