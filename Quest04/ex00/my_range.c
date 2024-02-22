c4b8381d4130eee7eb36dd272809b3da89247c8a
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-02-22 10:01:22 +0000

    setup completed

---
+++ Quest04/ex00/my_range.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// create a function my_range, returns a malloc'd array ofi ntegerrs
+
+//  This integer array should contain values between min and max
+
+// Min included - max excluded
+
+//  eg: In: 1 && 4, Out: [1, 2, 3]
+
+int* my_range(int param_1, int param_2){
+
+
+    // return malloc'd array
+}
+
+int test() {
+
+    int input_1a = 1;
+    int input_1b = 4;
+
+    int input_2a = 7;
+    int input_2b = 10;
+
+    int input_3a = 10;
+    int input_3b = 11;
+
+    printf("Test 1: input_1a && input_1b -> %d", my_range(input_1a, input_1b));
+
+    printf("Test 2: input_2a && input_2b -> %d", my_range(input_2a, input_2b));
+
+
+    printf("Test 3: input_3a && input_3b -> %d", my_range(input_3a, input_3b));
+
+    return 0;
+}
+
+int main(){
+    test();
+    return 0;
+}
