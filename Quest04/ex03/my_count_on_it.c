d5bec0df29efd7d5bdeadd6a663ccb6f33fcb5e7
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-03-06 13:54:32 +0000

    next step is to alloc memory for the integer array

--- Quest04/ex03/my_count_on_it.c
+++ Quest04/ex03/my_count_on_it.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Create a function my_count_on_it that recievs a string array as a parameter and returns an array with the length of each string
 
@@ -19,27 +20,35 @@ typedef struct s_integer_array {
 #endif
 
 integer_array* my_count_on_it(string_array* param_1){
-    integer_array *result; //allocate memory for integer struct
+    // you have to use malloc to create the allocated space in the memory in order to assign the arrays
+
+    integer_array* result; //allocate memory for integer struct
     result->size = param_1->size;
 
     for(int i = 0; i < param_1->size;i++){
         char *str = param_1->array[i]; //allocate memory for the array in string array
+        int j = 0;
         while(*str != 00){
             // iterates through each character in the string at index i
+            j++;
         }
+        result->array[i] = j;
     }
-
+    return result;
 }
 
 int main() {
     string_array *input_1;
     input_1->size = 4;
+    input_1->array = (char**)malloc(input_1->size * sizeof(char*));
     input_1-> array[0] = "This";
     input_1-> array[1] = "is";
     input_1-> array[2] = "the";
     input_1-> array[3] = "way";
 
     my_count_on_it(input_1);
+    free(input_1->array); // malloc and free require <stdblib.h>
+
     return 0;
 }
 
@@ -53,3 +62,5 @@ int main() {
 
 // Input: ["aBc"]
 // Output: [3]
+
+//  Find a way of debugging this program gdb might be the best option 
