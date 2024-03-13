c3eed76246e419d389f342f58b5ef37d0d9acd92
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-03-13 12:07:54 +0000

    Started again from the beginning

--- Quest04/ex04/my_join.c
+++ Quest04/ex04/my_join.c
@@ -17,78 +17,103 @@ typedef struct s_string_Array {
 } string_array;
 #endif
 
+int main(){
+
+    char* string[] = {
+        "Hello",
+        "World",
+        "!"
+    };
+
+    int size_1 = 3;
+
+    char** result = (char**)malloc(size_1*sizeof(char**));
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 // Do I need to allocate memory for this?
 
 // How do you determine the size of space that malloc should allocate?
 
-void my_strcat(char* dest, const char* src){
+// void my_strcat(char* dest, const char* src){
 
-    while(*dest != '\0'){
-        dest++;
-    } // get to the end of the character array
+//     while(*dest != '\0'){
+//         dest++;
+//     } // get to the end of the character array
 
-    while(*src != '\0'){
-        *dest = *src; //now at the end of the charcater array, set it to the first character of the src array
-        dest++; // move on by 1 from that position
-        src++; // move to the next character in the src array
-    } 
+//     while(*src != '\0'){
+//         *dest = *src; //now at the end of the charcater array, set it to the first character of the src array
+//         dest++; // move on by 1 from that position
+//         src++; // move to the next character in the src array
+//     } 
 
-    *dest = '\0'; // at the end, declare a null terminator
-}
+//     *dest = '\0'; // at the end, declare a null terminator
+// }
 
-char* my_join(string_array* array, char* sep_char){
+// char* my_join(string_array* array, char* sep_char){
 
-    int char_nums = 0; // counter for characters
-    char concat_str[] =  "";
-    // concat_str[char_nums];
+//     int char_nums = 0; // counter for characters
+//     char concat_str[] =  "";
+//     // concat_str[char_nums];
 
-    for(int i = 0; i < array->size;i++){
-        char* str = strdup(array->array[i]);
+//     for(int i = 0; i < array->size;i++){
+//         char* str = strdup(array->array[i]);
 
-        while(*str != '\0'){
-            char_nums++;
-            str++;
-        }
+//         while(*str != '\0'){
+//             char_nums++;
+//             str++;
+//         }
 
-        while(*sep_char != '\0'){
-            char_nums++;
-            // concat_str[*sep_char] TRYING TO FILL THIS EMPTY ARRAY WITH THE CHARACTERS OF THE TWO ARGUMENTS AS THEY LOOP THROUGH
-            sep_char++;
-        }
+//         while(*sep_char != '\0'){
+//             char_nums++;
+//             // concat_str[*sep_char] TRYING TO FILL THIS EMPTY ARRAY WITH THE CHARACTERS OF THE TWO ARGUMENTS AS THEY LOOP THROUGH
+//             sep_char++;
+//         }
 
-        my_strcat(concat_str, array->array[i]);
-    // we need to determine the size of character in sep_char
+//         my_strcat(concat_str, array->array[i]);
+//     // we need to determine the size of character in sep_char
 
-    } // determine nunmber of characters
+//     } // determine nunmber of characters
 
 
-    // strcat() ?
+//     // strcat() ?
 
-    char* result = (char*)malloc(char_nums*sizeof(char));
-     // how many characters do we make space for?
+//     char* result = (char*)malloc(char_nums*sizeof(char));
+//      // how many characters do we make space for?
     
-    result = concat_str;
+//     result = concat_str;
 
-    return result;
-}
+//     return result;
+// }
 
-int main(){
+// int main(){
 
-    string_array* test_1 = (string_array*)malloc(sizeof(string_array));
-    test_1->size = 4;
-    test_1->array = (char**)malloc(test_1->size*sizeof(char*));
+//     string_array* test_1 = (string_array*)malloc(sizeof(string_array));
+//     test_1->size = 4;
+//     test_1->array = (char**)malloc(test_1->size*sizeof(char*));
 
-    test_1->array[0] = strdup("Hello");
-    test_1->array[1] = strdup("World");
-    test_1->array[2] = strdup("!");
+//     test_1->array[0] = strdup("Hello");
+//     test_1->array[1] = strdup("World");
+//     test_1->array[2] = strdup("!");
 
-    char* result = my_join(test_1, "");
+//     char* result = my_join(test_1, "");
 
-    printf("Result for Test 1 -> %s\n", result);
+//     printf("Result for Test 1 -> %s\n", result);
 
-    free(test_1);
-    free(result);
+//     free(test_1);
+//     free(result);
     
 
-    return 0;
-}
+//     return 0;
+// }
