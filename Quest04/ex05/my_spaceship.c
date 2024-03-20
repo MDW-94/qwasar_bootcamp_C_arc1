20cf0de5b87d62194123ecc19fde118769e59d50
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-03-20 12:56:00 +0000

    starting to set up input handling

--- Quest04/ex05/my_spaceship.c
+++ Quest04/ex05/my_spaceship.c
@@ -25,26 +25,71 @@
 #ifndef STRUCT_DIRECTION_ARRAY
 #define STRUCT_DIRECTION_ARRAY
 typedef struct s_direction_array {
-    int size;
-    // array of integers and a string?
-} direction_array;
+    int x_value; // set to 0 at beginning of function
+    int y_value; // set to 0 at beginning of function
+    char** orientation; // initialise with ['up', 'right', 'down', 'left']
+} ship_position;
 #endif
 
 
 
 char* my_spaceship(char* param_1){
+
+    char lit_orient[4][5] = {'up', 'right', 'down', 'left'}; //variable[no. of string][length of string]
+
+    ship_position* position = (ship_position*)malloc(sizeof(ship_position));
+    int struct_size = 4; // four arrays of directions
+    position->orientation = (char**)malloc(struct_size*sizeof(char*)); // mem allocation for 4 orientation arrays
+
+    position->x_value = 0;
+    position->y_value = 0;
+
+    // Automated: (for loop, strlen, strcpy)
+    for(int i = 0; i < 4; i++){ // <= 3?
+        int orient_size = my_strlen(lit_orient[i]);
+        position->orientation[i] = (char*)malloc(orient_size*sizeof(char));
+        
+    }
+    
+    
+    // Hardcoded:
+    position->orientation[0] = (char*)malloc(2*sizeof(char)); // 'up' extra for null?
+    position->orientation[1] = (char*)malloc(5*sizeof(char)); // 'right'
+    position->orientation[2] = (char*)malloc(4*sizeof(char)); // 'down'
+    position->orientation[3] = (char*)malloc(4*sizeof(char)); // 'left'
+
+    position->orientation[0] = 'up';
+    position->orientation[1] = 'right';
+    position->orientation[2] = 'down';
+    position->orientation[3] = 'left';
+
     // INPUT: "RRAALLAA" string of direcitons
 
-    // OUTPUT: String w array of x,y coords and orientation
+ 
 
-    int result_size = 0; 
-
-    // NEEDS TO BE DETERMINED
+    int result_size = 0; //sum of all static characters in result string + dynamic characters in result in string
 
     char* result_str = (char*)malloc(result_size*sizeof(char));
 
+
+    // OUTPUT: String w array of x,y coords and orientation
+
+    // sprintf to store all data inside a string
+
     return result_str;
 }
+
+
+// int main() {
+//     int num1 = 10, num2 = 20;
+//     char str[30]; // Allocate space for the resulting string
+
+//     sprintf(str, "The numbers are: %d and %d", num1, num2);
+
+//     printf("%s\n", str);  // Output: The numbers are: 10 and 20
+
+//     return 0;
+// }
 
 int main() {
     return 0;
