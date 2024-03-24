2c4defe865b918c2380d160e11eb97f7562afa64
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-03-24 13:03:02 +0000

    finalised

--- Quest04/ex05/my_spaceship.c
+++ Quest04/ex05/my_spaceship.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Design a simulator for a spaceship.
-// This simulator create a virtual "space" and kee;s track of the basic movements and directions of a given ship
-
-// Your job is to keep track of where the ship is and it's orientation relative to its starting point
-
-// The simulator must take in a string of letters that represent a planned flight path for the spaceship
-
-// "RRALALAA is interpretted as Turn right, turn right, advance, turn left, advance, advance"
-
-// Once given this initial flight path, your program must return the x,y coordinates of a ship's final destination as well as it's orientation relative to the starting point
-
-// Orientation is represented as eft, right, up or down
-
-// Space is infinite, so the x,y coordinates you return could be placed on a seemingly infiinite grid and can be negatieve or positive values
-
-// EG upward facing ship at 0,0 - takes "RRALAA" - the finish will be 2, -1 and orientation will be right (facing right)
-
-// OUTPUT: "{x: X, y: Y, direction: 'DIRECTION'}" - struct?
-// This function returns a STRING
-
-// you can use 'snprintf'
-
 #ifndef STRUCT_DIRECTION_ARRAY
 #define STRUCT_DIRECTION_ARRAY
 typedef struct s_direction_array {
@@ -49,13 +27,11 @@ char* my_spaceship(char* str_og){
         return NULL; // or output format default?
     }
 
-
-    char lit_orientation[4][5 + 1] = {"up", "right", "down", "left"}; //variable[no. of string][length of string] - length of string needs +1
-
+    char lit_orientation[4][5 + 1] = {"up", "right", "down", "left"};
     ship_position* position = (ship_position*)malloc(sizeof(ship_position));
 
     // X, Y Values:
-    position->x_value = 0; // these become our counters
+    position->x_value = 0;
     position->y_value = 0;
 
     // Orientation:
@@ -64,13 +40,8 @@ char* my_spaceship(char* str_og){
 
     // Automated: (for loop, strlen, strcpy)
     for(int i = 0; i < struct_size; i++){ // <= 3?
-        // Pt. 1
-        // int orient_size = my_strlen(lit_orientation[i]);
-        position->orientation[i] = (char*)malloc((my_strlen(lit_orientation[i])) + 1*sizeof(char)); // mem allocated, '+1' for null terminator?
-
-        // Pt. 2
+        position->orientation[i] = (char*)malloc((my_strlen(lit_orientation[i])) + 1*sizeof(char)); // mem allocated, '
         position->orientation[i] = lit_orientation[i]; 
-        // assign string literal to array position - potential problem? If so use strcpy to iterate through string literal?
     }
 
         // Hardcoded:
@@ -107,9 +78,7 @@ char* my_spaceship(char* str_og){
     printf("str_og_size variable %i\n", str_og_size);
     printf("\n");
     for(int i = 0; i < str_og_size;i++){ 
-        // iterate for amount of char in input string
-        // remember to dereference the pointer!
-        char* char_address = str_og + i; //pointer is incremented according to value of iteration
+        char* char_address = str_og + i;
         char_at_i = *char_address;
         printf("char_at_i initialised with %c\n", char_at_i);
 
@@ -214,10 +183,7 @@ char* my_spaceship(char* str_og){
     printf("Testing // Updated Struct y_value: %i\n", position->y_value);
     printf("Testing // Updated Struct orientation value: %s\n", position->orientation[orientation_counter]);
 
-
-    // Now the struct x, y values are updated && we have the index reference for which position the ship is facing
-
-    // "{x: X, y: Y, direction: 'DIRECTION'}" - strlen?
+    // "{x: X, y: Y, direction: 'DIRECTION'}"
  
     int result_size = my_strlen("{x: X, y: Y, direction: 'right'}"); //sum of all static characters in result string + dynamic characters in result in string
 
@@ -236,21 +202,9 @@ char* my_spaceship(char* str_og){
     return result_str;
 }
 
-
-// int main() {
-//     int num1 = 10, num2 = 20;
-//     char str[30]; // Allocate space for the resulting string
-
-//     sprintf(str, "The numbers are: %d and %d", num1, num2);
-
-//     printf("%s\n", str);  // Output: The numbers are: 10 and 20
-
-//     return 0;
-// }
-
 int main() {
 
-    char* test_1 = (char*)malloc(10*sizeof(char)); // 10 just a random amount, could be more accurate
+    char* test_1 = (char*)malloc(10*sizeof(char));
     // test_1 = "RAALLRA";
     // test_1 = "RRALAA";
     test_1 = "RAALALL";
@@ -260,5 +214,3 @@ int main() {
 
     return 0;
 }
-
-//https://carbon.now.sh/ - CREATE VERSIONS OF CODE TO PRESENT
