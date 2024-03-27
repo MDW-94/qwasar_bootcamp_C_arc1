4a302c76a51be05040f30c745a6ed15661f8bb67
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-03-27 13:26:36 +0000

    setting up my_square project file

---
+++ my_square.c/my_square.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#ifndef CHAR_SQUARE_ARRAY
+#define CHAR_SQUARE_ARRAY
+typedef char_square_array {
+    int size;
+    char** array_strings;
+}
+#endif
+
+
+
+int main(){
+
+    return 0;
+}
