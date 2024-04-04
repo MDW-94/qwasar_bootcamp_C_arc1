dcbaec3e7dba6dec76795f182ffff48f9e7bb408
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-04-04 13:06:31 +0100

    Started Quest05

---
+++ Quest05/ex00/my_count_files.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char** av){
+
+    return 0;
+}
+
+
+
+// Exercise: my_count_files
+
+// Write a command line that counts and displays the number of regular files and directories in the current directory and all its sub-directories. It should include ".", the starting directory.
+
+// Tips
+// You should dig into the command ls (man ls)
+// You should dig into the command wc (man wc)
