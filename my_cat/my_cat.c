24fbc8230848d1dd516c4e0558834211b58dd1ee
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-04-15 13:50:27 +0100

    qwasar assignment version

--- my_cat/my_cat.c
+++ my_cat/my_cat.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+// #include <string.h>
 
 int my_cat(char* fileName, FILE** readFile){
     int c;
@@ -16,8 +16,8 @@ int my_cat(char* fileName, FILE** readFile){
         fclose(*readFile);
     }
 
-    printf("---------------------");
-    printf("\nThe function has successfully read the file\n");
+    // printf("---------------------");
+    // printf("\nThe function has successfully read the file\n");
     // Remember to close the file with fclose()
     return 1;
 }
@@ -28,38 +28,37 @@ int main(int ac, char** av){
 
     FILE* fileToBeRead;
 
-    char filename[100];
-    printf("Enter the filename to open \n");
-    scanf("%s", filename);
-    printf("%s\n", filename);
-    printf("%s <- outputted as string form\n", av[1]);
-    printf("---------------------\n");
+    // char filename[100];
+    // printf("Enter the filename to open \n");
+    // scanf("%s", filename);
+    // printf("%s\n", filename);
+    // printf("%s <- outputted as string form\n", av[1]);
+    // printf("---------------------\n");
 
     // THIS IS ENOUGH FOR THE ASSIGNMENT! 
 
-    // for(int i = 1; i < ac; i++){ 
-    //         if(my_cat(av[i], &fileToBeRead) != 1){
-    //     printf("\n %s: Error opening file.\n", __FUNCTION__);
-    // } else {
+    for(int i = 1; i < ac; i++){ 
+            if(my_cat(av[i], &fileToBeRead) != 1){
+        printf("\n %s: Error opening file.\n", __FUNCTION__);
+    } 
+    // else {
     //     // post function code
     //     printf("File read successully\n");
     //     printf("---------------------\n");
     // }
-    // }
-
-    for(int i = 1; i < ac; i++){
-        if(strcmp(av[i], filename) == 0){
-
-            if(my_cat(av[i], &fileToBeRead) != 1){
-            printf("\n %s: Error opening file.\n", __FUNCTION__);
-                } else {
-                // post function code
-                printf("File read successully\n");
-                printf("---------------------\n");
-            }
-            break;
-        }
     }
+
+    // for(int i = 1; i < ac; i++){
+    //     if(strcmp(av[i], filename) == 0){
+    //         if(my_cat(av[i], &fileToBeRead) != 1){
+    //         printf("\n %s: Error opening file.\n", __FUNCTION__);
+    //             } else {
+    //             printf("File read successully\n");
+    //             printf("---------------------\n");
+    //         }
+    //         break;
+    //     }
+    // }
     return 0;
 }
 
