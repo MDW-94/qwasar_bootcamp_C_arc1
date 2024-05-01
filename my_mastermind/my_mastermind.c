b3da537a2678b79f578a0a561584b865b6e4b795
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-05-01 17:17:25 +0100

    added time in to game for potential usage

--- my_mastermind/my_mastermind.c
+++ my_mastermind/my_mastermind.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <time.h>
 
+#ifndef GAME_SETTINGS
+#define GAME_SETTINGS
+typedef struct game_settings {
+    char* secret_code;
+    int n_attempts;
+} settings;
+#endif
+
+
+// int ac, char** av
 int main(){
 
-    printf("Hello, my_mastermind\n");
+    time_t sec;
+    time(&sec);
+
+    // Array of numbers "Pieces"
+    const int pieces_size = 9;
+    char pieces[pieces_size] = {'0','1','2','3','4','5','6','7','8'};
+
+    char secret_code[4];
+    // Secret code = n1,n2,n3,n4 where n is an int from array || specified code av[x] == '-c x'
+    // int attempts = default: 10 || specfiied av[x] == '-t x'
+
+
+    // switch(){} for multiple if statements?
+
+    // default: // 
+
+
+
+    printf("\nHello, my_mastermind -> initializing...\n");
+    printf("Time: %.24s\n\n", ctime(&sec));
+
+    for(int i = 0; i < pieces_size;i++){
+            printf("Piece %i -> %c\n", i, pieces[i]);
+    }
+
+    printf("Secret Code: %s\n", secret_code);
+
+
     return 0;
 }
+
+
+// https://www.geeksforgeeks.org/time-function-in-c/
