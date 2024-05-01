ccfbd6d58b05df52597de98e95fb4756865f01f1
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-05-01 17:42:53 +0100

    segfault

--- my_mastermind/my_mastermind.c
+++ my_mastermind/my_mastermind.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-#ifndef GAME_SETTINGS
-#define GAME_SETTINGS
-typedef struct game_settings {
-    char* secret_code;
-    int n_attempts;
-} settings;
-#endif
+// #ifndef GAME_SETTINGS
+// #define GAME_SETTINGS
+// typedef struct game_settings {
+//     char* secret_code;
+//     int n_attempts;
+// } settings;
+// #endif
 
 
 // int ac, char** av
-int main(){
+int main(int ac, char** av){
 
     time_t sec;
     time(&sec);
@@ -21,12 +23,20 @@ int main(){
     char pieces[pieces_size] = {'0','1','2','3','4','5','6','7','8'};
 
     char secret_code[4];
+
+        if(ac > 0){
+        char code_param[2]; *code_param = atoi(av[1]);
+
+        if(strcmp(code_param, "-c") && av[2]){
+            *secret_code = atoi(av[2]);
+        }
+
+        // char attempt_param[1] = atoi(av[3]);
+    }
     // Secret code = n1,n2,n3,n4 where n is an int from array || specified code av[x] == '-c x'
     // int attempts = default: 10 || specfiied av[x] == '-t x'
 
-
     // switch(){} for multiple if statements?
-
     // default: // 
 
 
