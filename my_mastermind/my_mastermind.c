560d9248877f6237f857617fb50c4eba37f5475f
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-05-01 18:15:07 +0100

    basic setup done. Much more work to be completed

--- my_mastermind/my_mastermind.c
+++ my_mastermind/my_mastermind.c
@@ -26,10 +26,14 @@ int main(int ac, char** av){
     time_t sec;
     time(&sec);
 
+    printf("\n=========================================\n");
+    printf("\nHello, my_mastermind -> initializing...\n");
+    printf("Time: %.24s\n", ctime(&sec));
+    printf("\n=========================================\n");
+
     // Array of numbers "Pieces"
     const int pieces_size = 9;
     char pieces[pieces_size] = {'0','1','2','3','4','5','6','7','8'};
-
     char secret_code[4];
 
         if(ac > 1){ //if there are any args in run command
@@ -55,22 +59,18 @@ int main(int ac, char** av){
         // secret_code[2] = rand();
         // secret_code[3] = rand();
          }
+
+        
+    printf("Secret Code: %s\n", secret_code);
     // Secret code = n1,n2,n3,n4 where n is an int from array || specified code av[x] == '-c x'
     // int attempts = default: 10 || specfiied av[x] == '-t x'
 
     // switch(){} for multiple if statements?
     // default: // 
 
-
-
-    printf("\nHello, my_mastermind -> initializing...\n");
-    printf("Time: %.24s\n\n", ctime(&sec));
-
     for(int i = 0; i < pieces_size;i++){
             printf("Piece %i -> %c\n", i, pieces[i]);
     }
-
-    printf("Secret Code: %s\n", secret_code);
 
 
     return 0;
