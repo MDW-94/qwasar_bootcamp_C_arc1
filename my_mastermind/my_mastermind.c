bc78599c16807f1bea8374d121f691677859b3de
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-05-03 11:52:09 +0100

    setup variable defining number of rounds to be played

--- my_mastermind/my_mastermind.c
+++ my_mastermind/my_mastermind.c
@@ -40,6 +40,17 @@ char generate_secret_code(int ac, char** av, const int pieces_size, char secret_
     return *secret_code;
 }
 
+int initialize_rounds(int ac, char** av){
+    if(ac > 1){
+        char code_param[2]; *code_param = atoi(av[3]);
+        if(strcmp(code_param, "-t") && av[4]){
+            return atoi(av[4]);
+        }
+
+    }
+    return 10;
+}
+
 
 // int ac, char** av
 int main(int ac, char** av){
@@ -54,8 +65,9 @@ int main(int ac, char** av){
 
     // Array of numbers "Pieces"
     const int pieces_size = 9;
-    char pieces[pieces_size] = {'0','1','2','3','4','5','6','7','8'};
+    char pieces[pieces_size] = {'0','1','2','3','4','5','6','7','8'}; // necessary?
     char secret_code[4];
+    int rounds_declared = 0;
 
     generate_secret_code(ac, av, pieces_size, secret_code);        
     printf("Secret Code: %s\n", secret_code);
@@ -73,6 +85,14 @@ int main(int ac, char** av){
     for(int i = 0; i < pieces_size;i++){
             printf("Piece %i -> %c\n", i, pieces[i]);
     }
+
+    printf("\n=========================================\n");
+    printf("\nStarting Game...\n\n");
+
+    rounds_declared= initialize_rounds(ac, av);
+
+    printf("\nRounds Declared - > %i\n", rounds_declared);
+
 
 
     return 0;
