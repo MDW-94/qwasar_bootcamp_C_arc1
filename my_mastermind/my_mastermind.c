980eea9c72a923a00d91dc2d1867cba129fd8a8a
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-05-09 12:06:05 +0100

    need to add inputs into makefile

--- my_mastermind/my_mastermind.c
+++ my_mastermind/my_mastermind.c
@@ -3,22 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-// #define RAND_MAX 9
-
-// #ifndef GAME_SETTINGS
-// #define GAME_SETTINGS
-// typedef struct game_settings {
-//     char* secret_code;
-//     int n_attempts;
-// } settings;
-// #endif
-
-// char* generate_code(){
-//     char* result_code[4] = {'1','2','3','4'};
-//     return result_code;
-
-// }
-
 char generate_secret_code(int ac, char** av, const int pieces_size, char secret_code[]){
       if(ac > 1){ //if there are any args in run command
         char code_param[1]; *code_param = atoi(av[1]);
@@ -70,6 +54,7 @@ int check_player_answer(const char* answer){
 }
 
 void determine_diff(char* answer, char* secret_code){
+    // SETUP
     int well_placed     = 0;
     int misplaced       = 0;
     int index           = 0;
@@ -77,41 +62,40 @@ void determine_diff(char* answer, char* secret_code){
     int check_code[4]   = {0,0,0,0};
     int refresh         = 0;
 
-    // white peg, black peg, no peg: black pegs match, white pegs wrong order, blank -> 3 possibilities
-  
     // PT.1
     while(answer[index] != '\0'){
         if(answer[index] == secret_code[index] && refresh != 1){
-
-            printf("WP Found at index -> %i\n", index);
+            // printf("WP Found at index -> %i\n", index);
             well_placed++;
-
-            check_answer[index] = 1;
-            check_code[index] = 1;
-
+            check_answer[index]     = 1;
+            check_code[index]       = 1;
         }
-        printf("\nWhile 1 Index Count: %i\n", index);
+        // printf("\nWhile 1 Index Count: %i\n", index);
         index++;
     }
 
-    index = 0;
+    // RESET INDEX
+    index               = 0;
 
     // PT.2
     while(answer[index] != '\0'){
         if(answer[index] != secret_code[index]){
-            int j = 0;
+            int j       = 0;
             while(secret_code[j] != '\0'){
-                if(answer[index] == secret_code[j] && j != index && check_code[j] != 1 && check_answer[index] != 1){
-                    printf("Misplaced found at index -> %i | j -> %i\n", index, j);
+                if(answer[index] == secret_code[j] 
+                && j != index 
+                && check_code[j] != 1 
+                && check_answer[index] != 1){
+                    // printf("Misplaced found at index -> %i | j -> %i\n", index, j);
                     misplaced++;
                     check_answer[index] = 1;
-                    check_code[j] = 1;
-                    j = 3;
+                    check_code[j]       = 1;
+                    j                   = 3;
                 }
                 j++; 
             }
         }
-        printf("\nWhile 2 Index Count: %i\n", index);
+        // printf("\nWhile 2 Index Count: %i\n", index);
         index++;
     }
 
@@ -123,14 +107,14 @@ void determine_diff(char* answer, char* secret_code){
 }
 
 void game_engine(char secret_code[], int rounds_declared){
-    int round_index = 0;
-    int answer_check = 0;
+    int round_index     = 0;
+    int answer_check    = 0;
     printf("Rounds Declared -> %i\n", rounds_declared);
     printf("Secret Code -> %s\n", secret_code);
     while(round_index < rounds_declared && answer_check != 1){
         printf("\n=========================================\n");
         printf("\nRound %i\n", round_index);
-        printf("\nSecret Code -> %s\n", secret_code);
+        // printf("\nSecret Code -> %s\n", secret_code);
 
         // TAKE USER INPUT
        char player_answer[4];
