6c3c255b493c012021a8b15e9823758a3488548d
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-06-07 11:28:34 +0100

    determining how many bytes to read through write as special characters will need to be considered

--- my_printf/my_printf.c
+++ my_printf/my_printf.c
@@ -4,15 +4,34 @@
 #include <stdarg.h>
 
 int my_printf(const char* restrict format, ...){
-    int count = 0; // Needs to be initialized to no. of chars in char array
+    int count = 0; // Needs to be initialized to no. of chars in char array - my_strlen?
     va_list args;
     va_start(args, count);
 
     // enough space for end terminator:
-    char* total = malloc((count + 1)*sizeof(char)); 
-    if(total == NULL){return NULL;};
+    char* output_str = malloc((count + 1)*sizeof(char)); 
+    if(output_str == NULL){return NULL;};
 
-    
+    for(int i = 0;i<count;i++){
+        // traverse va_list, fetch ints, convert to char data types:
+        char ch = va_arg(args, int);
+        // store in final output string:
+        output_str[i] = ch;
+    }
+    output_str[count] = '\0';
+    va_end(args);
+
+    while(*output_str++ != '\0'){
+        write(1, output_str, 1);
+        //write(1, output_str, my_strlen(output_str)); THIS WAY MIGHT NOT WORK DUE TO NOT CONSIDERING SPECIAL CHARACTERS?
+    }
+
+
+
+    // return output_str? Printf just outputs to the terminal, return is either success, failure or EOF??
+
+    return 0;
+
 }
 
 int main(){
