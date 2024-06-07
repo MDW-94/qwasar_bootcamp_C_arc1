637b8f5f4670b48d8c366e17fe291fea3efa2059
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-06-07 11:44:49 +0100

    resolved error message, inputted the last named parameter before ellipsis in the my_printf function

--- my_printf/my_printf.c
+++ my_printf/my_printf.c
@@ -7,14 +7,13 @@ int my_strlen(char* str){
     int i = 0;char* temp_ptr = str;while(*temp_ptr++ != '\0'){i++;}return i;
 }
 
-int my_printf(char* restrict format, ...){
-    int count = 0;
-    count = my_strlen(format); 
+int my_printf(char* restrict input_str, ...){
     // Needs to be initialized to no. of chars in char array - my_strlen?
     va_list args;
-    va_start(args, count);
+    va_start(args, input_str);
 
     // enough space for end terminator:
+    int count = my_strlen(input_str); 
     char* output_str = malloc((count + 1)*sizeof(char)); 
     if(output_str == NULL){return 0;};
 
@@ -39,6 +38,8 @@ int my_printf(char* restrict format, ...){
 
     // return output_str? Printf just outputs to the terminal, return is either success, failure or EOF??
 
+    //free memory after completion
+    free(output_str);
     return 0;
 
 }
