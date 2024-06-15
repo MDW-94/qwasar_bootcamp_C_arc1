9c0006e5aa3bb8fe9336c95cbfd90c65693813ad
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-06-15 18:12:26 +0100

    cryptographic hash challenge for qwasar started

---
+++ c_playground/cryptographic_hash.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void sha256(const char* message, uint32_t hash[8]);
+
+int main(){
+
+    return 0;
+}
+
+void sha256(const char* message, uint32_t hash[8]){
+
+}
