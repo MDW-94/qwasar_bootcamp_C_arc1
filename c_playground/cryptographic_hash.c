37b46aae79a014774c24de793010a1b7e17bf8b0
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-06-15 19:06:18 +0100

    added pdf and notes to project

--- c_playground/cryptographic_hash.c
+++ c_playground/cryptographic_hash.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// SHA-256 Algorithm
+
 void sha256(const char* message, uint32_t hash[8]);
 
 int main(){
@@ -11,3 +13,13 @@ int main(){
 void sha256(const char* message, uint32_t hash[8]){
 
 }
+
+
+// https://xorbin.com/tools/sha256-hash-calculator?utm_content=cmp-true
+// https://infosecwriteups.com/breaking-down-sha-256-algorithm-2ce61d86f7a3
+// https://blog.daisie.com/sha-256-explained-comprehensive-secure-hash-tutorial/
+// https://github.com/EddieEldridge/SHA256-in-C
+// https://www.ssldragon.com/blog/sha-256-algorithm/
+// https://rbtblog.com/posts/SHA256-Algorithm-Implementation-in-C/#32-bit
+// https://blog.boot.dev/cryptography/how-sha-2-works-step-by-step-sha-256/
+// https://en.wikipedia.org/wiki/SHA-2
