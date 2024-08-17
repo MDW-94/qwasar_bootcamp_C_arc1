7e80f3aea4589379e61b6b8832e65a48a570d5e5
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-08-17 11:55:27 +0100

    feat: added my_iterative_pow function

---
+++ Quest06/my_iterative_pow.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int my_iterative_pow(int x, int y){
+    int result = x;
+    if(y == 0) return result = 0;
+
+    for(int i = 0;i<y - 1;i++){
+        result *= x;
+    }
+
+    return result;
+}
+
+int main() {
+    printf("2 * 2 = %i\n", my_iterative_pow(2,2));
+    printf("4 * 4 = %i\n", my_iterative_pow(4,4));
+    printf("8 * 8 = %i\n", my_iterative_pow(8,8));
+    return 0;
+}
