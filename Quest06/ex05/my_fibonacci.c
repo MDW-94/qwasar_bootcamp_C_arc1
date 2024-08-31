4daf61c68189700b07af34eefe689e2af1b4929a
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-08-31 17:48:50 +0100

    text: added comments

--- Quest06/ex05/my_fibonacci.c
+++ Quest06/ex05/my_fibonacci.c
@@ -18,6 +18,7 @@ int my_fibonacci(int n){
     return my_fibonacci(n - 1) + my_fibonacci(n - 2);
 }
 
+
 int main(){
     printf("\n\nTest 1 -> %i\n", my_fibonacci(1));
     printf("\n\nTest 1 -> %i\n", my_fibonacci(2));
@@ -34,3 +35,19 @@ int main(){
 
 // Example: 
 //  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+
+// Base Case: ('if(n <= 1) return n);
+// If 'n' is 0 or 1, the function returns 'n' directly. This stops the recursion and provides the base values necessary for further calculations
+//  The bases cases of the fibonacci sequence are F(0) = 0 and F(1) = 1
+
+//  Recursive Case:
+// 'return my_fibonacci(n - 1) + my_fibonacci(n - 2);'
+// For any n > 1, the functino computes the sum of the Fibonacci numbers at positions n - 1 and n - 2
+ //The function calls itself with the arguments 'n-1' and 'n-2' calculating the Fibonacci numbers for those positions recursively
+
+// my_fibonacci(4)
+// = my_fibonacci(3) + my_fibonacci(2)
+// = (my_fibonacci(2) + my_fibonacci(1)) + (my_fibonacci(1) + my_fibonacci(0))
+// = ((my_fibonacci(1) + my_fibonacci(0)) + 1) + (1 + 0)
+// = ((1 + 0) + 1) + (1 + 0)
+// = 3
